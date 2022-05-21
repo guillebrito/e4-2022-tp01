12898cef06a559c7a789c65eaa588901e1933a53
guillebrito <guillermonbrito@gmail.com>
2022-05-21 02:29:29 -0300

    Copia memoria una palabra

--- test/test_ensamblador/test_ensamblador.c
+++ test/test_ensamblador/test_ensamblador.c
@@ -29,7 +29,7 @@ int main(void)
     RUN_TEST(copiaMemoria_debe_copiar_cero_bytes);
     RUN_TEST(copiaMemoria_debe_copiar_un_byte);
     RUN_TEST(copiaMemoria_debe_copiar_media_palabra);
-    // RUN_TEST(copiaMemoria_debe_copiar_una_palabra);
+    RUN_TEST(copiaMemoria_debe_copiar_una_palabra);
     // RUN_TEST(copiaMemoria_debe_copiar_una_palabra_mal_alineada);
     
     // RUN_TEST(copiaCadena_debe_copiar_una_cadena);
